06345a3856e51322a7183f28eee947b09950a304
ulysseskee <34738958+ulysseskee@users.noreply.github.com>
2022-01-08 03:22:44 +0800

    working react FE

--- attendancetracker/src/components/FrontPage.js
+++ attendancetracker/src/components/FrontPage.js
@@ -1,5 +1,17 @@
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Heading,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
+const _ = require("lodash");
 
 const getHistory = async () => {
   const url = `https://mongo-realm-worker.ulysseskcw96.workers.dev/api/attendance`;
@@ -13,6 +25,65 @@ const getHistory = async () => {
   return resp.json();
 };
 
+function getDate(str) {
+  const re = /(\d{2})(\d{2})(\d{4})/;
+  const match = str.match(re);
+  const d = match[1];
+  const m = match[2];
+  const y = match[3];
+  const dateToConvert = `${y}-${m}-${d}`;
+  const date = new Date(dateToConvert);
+  return date;
+}
+
+const DisplayTable = (props) => {
+  const dateResults = {};
+  props.data.forEach((docu) => {
+    let dateObj = getDate(docu.date);
+
+    if (dateResults.hasOwnProperty(docu.date) === false) {
+      // not in dateResults array, add to
+      dateResults[dateObj] = [];
+    }
+  });
+
+  const dateKeys = Object.keys(dateResults);
+  const sortedDateKeys = _.sortBy(dateKeys, function (dateObj) {
+    return new Date(dateObj);
+  });
+
+  props.data.forEach((docu) => {
+    let dateObj = getDate(docu.date);
+    dateResults[dateObj].push({ name: docu.name, status: docu.status });
+  });
+
+  console.log("dateRes", dateResults);
+  console.log(dateKeys);
+  console.log(sortedDateKeys);
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Date</Th>
+          <Th>Name</Th>
+          <Th>Status</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {sortedDateKeys.map((dKey) =>
+          dateResults[dKey].map((entry) => (
+            <Tr key={entry}>
+              <Td>{dKey}</Td>
+              <Td>{entry.name}</Td>
+              <Td>{entry.status}</Td>
+            </Tr>
+          ))
+        )}
+      </Tbody>
+    </Table>
+  );
+};
+
 const FrontPage = () => {
   const [historyData, setHistoryData] = useState([]);
 
@@ -20,6 +91,7 @@ const FrontPage = () => {
     async function fetchMyAPI() {
       const data = await getHistory();
       if (data) {
+        setHistoryData(data);
         console.log(data);
       }
     }
@@ -28,8 +100,11 @@ const FrontPage = () => {
 
   return (
     <Container maxW="container.xl" p={0}>
-      <Flex h="100vh" py={20}>
-        <Heading>YOOOO</Heading>
+      <Flex h="120vh" py={20}>
+        <VStack w="full">
+          <Heading>Attendance History</Heading>
+          <DisplayTable data={historyData} />
+        </VStack>
       </Flex>
     </Container>
   );
--- cloudflare-worker-rest-api-realm-atlas/api_tests/post.sh
+++ cloudflare-worker-rest-api-realm-atlas/api_tests/post.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ./variables.sh
 #curl -X POST "${worker_url}/api/todos" -H "authorization: ${api_key}" -H 'Content-Type: application/json' -d '{"todo":"'"${1}"'"}'
-curl -X POST "${worker_url}/api/attendance?dateStr=08012022&telegramID=111111" -H "authorization: ${api_key}" -H 'Content-Type: application/json' -d '{"entry":{"name": "'"${1}"'", "status": "'"${2}"'", "telegramID":"'"${3}"'","date":"'"${4}"'"}}'
+curl -X POST "${worker_url}/api/attendance?dateStr=11012022&telegramID=1202717167" -H 'Content-Type: application/json' -d '{"entry":{"name": "'"${1}"'", "status": "'"${2}"'"}}'
